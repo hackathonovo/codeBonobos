de781265508f726ccc8b1380e07407aa13bca5a8
Mak Muftic <mak.muftic@altima.hr>
2017-05-21 08:53:23 +0200

    dsadsa

--- Web/src/main/resources/static/app/controller.js
+++ Web/src/main/resources/static/app/controller.js
@@ -1,26 +1,9 @@
-app.controller('MainController', ['$scope', '$sce', '$http', '$route', 'UserFactory', 'NgMap', function ($scope, $sce, $http, $route, UserFactory, NgMap) {
+app.controller('MainController', ['$scope', '$sce', '$http', '$route', 'ActionFactory', 'NgMap', function ($scope, $sce, $http, $route, ActionFactory, NgMap) {
     // ==== MODELS ====
-    this.config = {
-        preload: "none",
-        sources: [
-            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-            {src: $sce.trustAsResourceUrl("http://static.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
-        ],
-        tracks: [
-            {
-                src: "http://www.videogular.com/assets/subs/pale-blue-dot.vtt",
-                kind: "subtitles",
-                srclang: "en",
-                label: "English",
-                default: ""
-            }
-        ],
-        autoPlay: true,
-        theme: {
-            url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
-        }
-    };
+
+    ActionFactory.getAllActive().then(function (response) {
+        $scope.allActive = response.data.response;
+    });
 
     // ==== INIT MODELS ====
 
@@ -155,10 +138,6 @@ app.controller('CurrentController', ['$scope', 'ActionFactory', function ($scope
     // ==== MODELS ====
     ActionFactory.getAllActive().then(function (response) {
         $scope.allActive = response.data.response;
-
-        $scope.allActive.forEach(function (e) {
-            e.expended = false;
-        })
     });
 
     $scope.dictPrio = {
